00204e8dbc4edf0d615caa702c97b7279f53129e
teakap <tasos.kapetzoglou@gmail.com>
2017-02-01 20:51:29 +0200

    Add showCompleted and searchText reducers

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,8 @@ module.exports = {
     alias: {
       jQuery: 'jquery',
       applicationStyles: 'app/styles/app.scss',
-      actions: 'app/actions/actions.jsx'
+      actions: 'app/actions/actions.jsx',
+      reducers: 'app/reducers/reducers.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
