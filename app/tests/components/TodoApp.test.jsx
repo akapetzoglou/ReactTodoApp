a528dc795103682076c74ee50e06c8901608bc85
teakap <tasos.kapetzoglou@gmail.com>
2017-02-01 12:51:30 +0200

    Add completed and created timestamps

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -19,13 +19,17 @@ describe('TodoApp', () => {
   	todoApp.handleAddTodo(todoText);
 
   	expect(todoApp.state.todos[0].text).toBe(todoText);
+    // Expect createdAt to be a number
+    expect(todoApp.state.todos[0].createdAt).toBeA('number');
    });
 
   it('should toggle completed value when handleToggle called', () => {
     var todoData = {
       id: 11,
       text: 'Test features',
-      completed: false
+      completed: false,
+      createdAt: 0,
+      completedAt: undefined
     };
     var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
 
@@ -38,5 +42,33 @@ describe('TodoApp', () => {
     // Verify that value changed
     expect(todoApp.state.todos[0].completed).toBe(true);
 
+    // Expect completedAt to be a number
+    expect(todoApp.state.todos[0].completedAt).toBeA('number');
+
   });
+
+  it('should toggle todo from completed to incomplete', () => {
+    var todoData = {
+      id: 11,
+      text: 'Test features',
+      completed: true,
+      createdAt: 0,
+      completedAt: 123
+    };
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+
+    todoApp.setState({todos: [todoData]});
+    // check that todos first item has completed value of false
+    expect(todoApp.state.todos[0].completed).toBe(true);
+
+    // call handleToggle with 11
+    todoApp.handleToggle(11);
+    // Verify that value changed
+    expect(todoApp.state.todos[0].completed).toBe(false);
+
+    // Expect completedAt to be a number
+    expect(todoApp.state.todos[0].completedAt).toNotExist();
+
+  });
+  //Test that when toggle from ture to flase, completedAt get removed
 });
