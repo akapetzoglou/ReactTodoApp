566f096cdb4b54ead23f5cc9a45947a328b71725
teakap <tasos.kapetzoglou@gmail.com>
2017-02-06 17:53:46 +0200

    Imroved testing and configure

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 var uuid = require('node-uuid');
 var moment = require('moment');
 
-var TodoList = require('TodoList');
+import TodoList from 'TodoList';
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
 var TodoAPI = require('TodoAPI');
@@ -38,17 +38,7 @@ var TodoApp = React.createClass({
 			searchText: searchText.toLowerCase()
 		});
 	},
-	handleToggle: function (id) {
-		var updatedTodos = this.state.todos.map( (todo) => {
-			if (todo.id === id) {
-				todo.completed = !todo.completed;
-				todo.completedAt = todo.completed ? moment().unix() : undefined;
-			}
 
-			return todo;
-		});
-		this.setState({todos: updatedTodos});
-	},
 	render: function () {
 		var {todos, showCompleted, searchText} = this.state;
 		var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
@@ -61,8 +51,8 @@ var TodoApp = React.createClass({
 					<div className="column small-centered small-11 medium-6 large-5">
 						<div className="container">
 							<TodoSearch onSearch={this.handleSearch} />
-							<TodoList todos={filteredTodos} onToggle={this.handleToggle} />
-							<AddTodo onAddTodo={this.handleAddTodo} />	
+							<TodoList />
+							<AddTodo onAddTodo={this.handleAddTodo} />
 						</div>
 					</div>
 				</div>
